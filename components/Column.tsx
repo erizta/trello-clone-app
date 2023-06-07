4a5342ec28d35546d1b7d07424ff57fb59af1eed
erizta <eriztaalifad@gmail.com>
2023-06-07 23:33:55 +0700

    implementing search  functionality

--- components/Column.tsx
+++ components/Column.tsx
@@ -2,6 +2,7 @@ import { PlusCircleIcon } from "@heroicons/react/24/solid";
 import React from "react";
 import { Draggable, Droppable } from "react-beautiful-dnd";
 import TodoCard from "./TodoCard";
+import { useBoardStore } from "@/store/BoardStore";
 
 type ColumnProps = {
   id: TypedColumn;
@@ -18,6 +19,8 @@ const idToColumnText: {
 };
 
 function Column({ id, todos, index }: ColumnProps) {
+  const [searchString] = useBoardStore((state) => [state.searchString]);
+
   return (
     <Draggable draggableId={id} index={index}>
       {(provided) => (
@@ -38,29 +41,42 @@ function Column({ id, todos, index }: ColumnProps) {
                 <h2 className="flex justify-between p-2 text-xl font-bold">
                   {idToColumnText[id]}
                   <span className="px-2 py-2 text-sm text-gray-500 bg-gray-200 rounded-full">
-                    {todos.length}
+                    {!searchString
+                      ? todos.length
+                      : todos.filter((todo) =>
+                          todo.title.toLowerCase().includes(searchString.toLowerCase())
+                        ).length}
                   </span>
                 </h2>
 
                 <div className="space-y-2">
-                  {todos.map((todo, index) => (
-                    <Draggable
-                      key={todo.$id}
-                      draggableId={todo.$id}
-                      index={index}
-                    >
-                      {(provided) => (
-                        <TodoCard
-                          todo={todo}
-                          index={index}
-                          id={id}
-                          innerRef={provided.innerRef}
-                          draggableProps={provided.draggableProps}
-                          dragHandleProps={provided.dragHandleProps}
-                        />
-                      )}
-                    </Draggable>
-                  ))}
+                  {todos.map((todo, index) => {
+                    if (
+                      searchString &&
+                      !todo.title
+                        .toLowerCase()
+                        .includes(searchString.toLowerCase())
+                    )
+                      return null;
+                    return (
+                      <Draggable
+                        key={todo.$id}
+                        draggableId={todo.$id}
+                        index={index}
+                      >
+                        {(provided) => (
+                          <TodoCard
+                            todo={todo}
+                            index={index}
+                            id={id}
+                            innerRef={provided.innerRef}
+                            draggableProps={provided.draggableProps}
+                            dragHandleProps={provided.dragHandleProps}
+                          />
+                        )}
+                      </Draggable>
+                    );
+                  })}
 
                   {provided.placeholder}
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,14 @@
 import Image from "next/image";
 import { UserCircleIcon, MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import Avatar from "react-avatar";
+import { useBoardStore } from "@/store/BoardStore";
 
 const Header = () => {
+  const [searchString, setSearchString] = useBoardStore((state) => [
+    state.searchString,
+    state.setSearchString,
+  ]);
+
   return (
     <header>
       <div className="flex flex-col items-center p-5 md:flex-row bg-gray-500/10 rounded-b-2xl">
@@ -26,6 +32,8 @@ const Header = () => {
               type="text"
               placeholder="Search"
               className="flex-1 p-2 outline-none"
+              value={searchString}
+              onChange={(e) => setSearchString(e.target.value)}
             />
             <button hidden type="submit">
               Search
